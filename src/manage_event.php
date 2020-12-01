683e16d386ed7eec9b0adbfb0d39cc4176468deb
Densik50 <DanielPaulovic@gmail.com>
2020-12-01 19:51:03 +0100

    few changes

--- src/manage_event.php
+++ src/manage_event.php
@@ -19,61 +19,100 @@
                     //write out manage panel for event
                     if(is_owner($conn, $_GET["id"], $_SESSION["UserID"]) || isset($_SESSION["admin"]))
                     {
-                        
+                        $event = event_exists_byid($conn, $_GET["id"]);
+                        $event_id = $_GET["id"];
+                        $eventname = $event["Name"];
+                        $describtion = $event["Describtion"];
+                        $Address = $event["Address"];
+                        $Start_date = $event["Start_date"];
+                        $Start_time = $event["Start_time"];
+                        $End_date = $event["End_date"];
+                        $End_time = $event["End_time"];
+                        $Price = $event["Price"];
+                        $MaxCapacity = $event["MaxCapacity"];
                         echo "
-                        <h2>Tickets for event_id:  ". $_GET["id"]."</h2>
                         <table class=\"table table-sm\">
-                            <thead>
-                                <tr>
-                                    <th scope=\"col\">TicketID</th>
-                                    <th scope=\"col\">UserID</th>
-                                    <th scope=\"col\">Username</th>
-                                    <th scope=\"col\">Paied</th>
-                                    <th scope=\"col\"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                        ";
-                        foreach($all_tickets as $ticket):
-                            $id = $ticket["TicketID"];
-                            $eventid = $ticket["EventID"];
-                            $userid = $ticket["Buyer"];
-                            $paied = $ticket["Paied"];;
-                            $user = user_exists_byid($conn, $userid);
-                            $username = $user["Username"];
-
-                            if($paied)
-                            {
-                                $paied_info = "true";
-                            }
-                            else
-                            {
-                                $paied_info = "false";
-                            }
-                            echo "
-                                <tr>
-                                    <th scope=\"row\">$id</th>
-                                    <td>$userid</td>
-                                    <td> <a href=\"profile.php?user=$username\">$username</a></td>
-                                    <td>$paied_info</td>";
-
-                                if(!$paied)
-                                    echo"
-                                    <td>
-                                    <form class=\"register-form\" action=\"php/includes/make_ticket_paied.include.php\" method=\"post\">
-                                        <input type=\"hidden\" id=\"ticket_id\" name=\"ticket_id\" value=\"$id\">
-                                        <input type=\"hidden\" id=\"eventid\" name=\"eventid\" value=\"$eventid\">
-                                        <!-- SUBMIT BUTTON -->
-                                        <button type=\"submit\" name=\"submit\" class=\"btn btn-primary\">Set as paied</button>
-                                        <!-- SUBMIT BUTTON END -->
-                                    </form>
-                                    </td>";
-                        echo    "
-                                </tr>
-                            ";
-                        endforeach;
-                        echo "
-                            </tbody>
+                            <tr>    
+                                <th scope=\"col\">Event name:</th>
+                                <td>$eventname</td>
+                                <td>
+                                    <button type=\"submit\" class=\"btn btn-primary\" id=\"Name\">
+                                        <a class=\"text-white\" href=\"edit_event.php?info=Name&event_id=$event_id\">Edit</a>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>    
+                                <th scope=\"col\">Describtion:</th>
+                                <td>$describtion</td>
+                                <td>
+                                    <button type=\"submit\" class=\"btn btn-primary abtn\" id=\"Describtion\">
+                                        <a class=\"text-white\" href=\"edit_event.php?info=Describtion&event_id=$event_id\">Edit</a>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>    
+                                <th scope=\"col\">Address:</th>
+                                <td>$Address</td>
+                                <td>
+                                    <button type=\"submit\" class=\"btn btn-primary abtn\" id=\"Address\">
+                                        <a class=\"text-white\" href=\"edit_event.php?info=Address&event_id=$event_id\">Edit</a>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>    
+                                <th scope=\"col\">Start_date:</th>
+                                <td>$Start_date</td>
+                                <td>
+                                    <button type=\"submit\" class=\"btn btn-primary abtn\" id=\"Start_date\">
+                                        <a class=\"text-white\" href=\"edit_event.php?info=Start_date&event_id=$event_id\">Edit</a>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>    
+                                <th scope=\"col\">Start_time:</th>
+                                <td>$Start_time</td>
+                                <td>
+                                    <button type=\"submit\" class=\"btn btn-primary abtn\" id=\"Start_time\">
+                                        <a class=\"text-white\" href=\"edit_event.php?info=Start_time&event_id=$event_id\">Edit</a>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>    
+                                <th scope=\"col\">End_date:</th>
+                                <td>$End_date</td>
+                                <td>
+                                    <button type=\"submit\" class=\"btn btn-primary abtn\" id=\"End_date\">
+                                        <a class=\"text-white\" href=\"edit_event.php?info=End_date&event_id=$event_id\">Edit</a>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>    
+                                <th scope=\"col\">End_time:</th>
+                                <td>$End_time</td>
+                                <td>
+                                    <button type=\"submit\" class=\"btn btn-primary abtn\" id=\"End_time\">
+                                        <a class=\"text-white\" href=\"edit_event.php?info=End_time&event_id=$event_id\">Edit</a>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>    
+                                <th scope=\"col\">Price:</th>
+                                <td>$Price</td>
+                                <td>
+                                    <button type=\"submit\" class=\"btn btn-primary abtn\" id=\"Price\">
+                                        <a class=\"text-white\" href=\"edit_event.php?info=Price&event_id=$event_id\">Edit</a>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>    
+                                <th scope=\"col\">MaxCapacity:</th>
+                                <td>$MaxCapacity</td>
+                                <td>
+                                    <button type=\"submit\" class=\"btn btn-primary abtn\" id=\"MaxCapacity\">
+                                        <a class=\"text-white\" href=\"edit_event.php?info=MaxCapacity&event_id=$event_id\">Edit</a>
+                                    </button>
+                                </td>
+                            </tr>
                         </table>
                         ";
                     }
